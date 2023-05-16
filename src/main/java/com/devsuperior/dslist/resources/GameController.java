ed950a81163ee3f6309202f7c276bbdd6d4a10b2
Gabrielle Lima <gabrielle.clima23@gmail.com>
2023-05-15 23:56:07 -0300

    Get game by id

--- src/main/java/com/devsuperior/dslist/resources/GameController.java
+++ src/main/java/com/devsuperior/dslist/resources/GameController.java
@@ -3,9 +3,11 @@ package com.devsuperior.dslist.resources;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 
@@ -28,6 +30,13 @@ public class GameController {
 	public GameController(GameService gameService) {
 		super();
 		this.gameService = gameService;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;
+		
 	}
 	
 	@GetMapping
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,7 +3,9 @@ package com.devsuperior.dslist.services;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -25,10 +27,20 @@ public class GameService {
 		super();
 		this.gameRepository = gameRepository;
 	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result =  gameRepository.getReferenceById(id);
+		GameDTO game = new GameDTO(result);
+		return game;
+	}
 
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
+	
+	
 
 }
